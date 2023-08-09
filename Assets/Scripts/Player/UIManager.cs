700e4bb6cb2438e00d45b0387dec93702b748885
jevs242 <ghostraidenpm@gmail.com>
2023-08-09 12:15:00 -0400

    Done

--- Assets/Scripts/Player/UIManager.cs
+++ Assets/Scripts/Player/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -63,4 +64,8 @@ public class UIManager : MonoBehaviour
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("PirateSeas");
+    }
 }
